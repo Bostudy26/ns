4d45bb0a2588f194f825edaa17764bbba77be534
Bo <boprojectbo@gmail.com>
2024-09-13 22:00:26 +0200

    Feat: Hero + ShoeCard

---
+++ src/components/Button.tsx
@@ -0,0 +1,23 @@
+interface ButtonProps {
+  label: string;
+  iconURL?: string;
+}
+const Button = ({
+  label,
+  iconURL,
+}: ButtonProps) => {
+  return (
+    <button
+      className="flex justify-center items-center gap-2 px-7 py-4 leading-none bg-coral-red rounded-full text-white border-coral-red"
+    >
+      {label}
+      <img 
+        src={iconURL}
+        alt="arrow-right"
+        className="ml-2 rounded-full w-5 h-5"
+      />
+    </button>
+  );
+};
+
+export default Button;
