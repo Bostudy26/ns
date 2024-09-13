3055b894a6ec1877bfcdfa633571c517ea8af4c3
Bo <boprojectbo@gmail.com>
2024-09-13 13:29:31 +0200

    Feat: Add sections folder+ CustomerReviews

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,44 @@
 
-function App() {
+import React from 'react'
 
+const App = () => {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <main className='relative'>
+     {/* Nav  */}
+      nav
+    <section className='xl:padding-l wide:padding-r padding-b'>
+      {/* Hero */}
+      hero
+    </section>
+    <section className='padding'>
+      {/* PopularProducts */}
+      Popular Products
+    </section>
+    <section className='padding'>
+      {/* SuperQuality */}
+      super Quality
+    </section>
+    <section className='padding-x py-10'>
+      {/* Services */}
+      Services
+    </section>
+    <section className='padding'>
+      {/* SpecialOffer */}
+      SpecialOffer
+    </section>
+    <section className='bg-pale-blue padding'>
+      {/* CustomerReviews */}
+      Customer Reviews
+    </section>
+    <section className='padding-x sm:py-32 py-16 w-full'>
+      {/* Subscribe */}
+      Subscribe
+    </section>
+    <section className=' bg-black padding-x padding-t pb-8'>
+      {/* Footer */}
+      Footer
+    </section>
+  </main>
   )
 }
 
