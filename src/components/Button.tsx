250a63f56ed4311fc8a0141a8482dba571f72f08
Bo <boprojectbo@gmail.com>
2024-09-14 13:22:06 +0200

    Feat: SuperQuality + components button edit

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,11 +11,11 @@ const Button = ({
       className="flex justify-center items-center gap-2 px-7 py-4 leading-none bg-coral-red rounded-full text-white border-coral-red"
     >
       {label}
-      <img 
-        src={iconURL}
-        alt="arrow-right"
-        className="ml-2 rounded-full w-5 h-5"
-      />
+        {iconURL && <img 
+          src={iconURL}
+          alt="arrow-right"
+          className="ml-2 rounded-full w-5 h-5"
+      />}
     </button>
   );
 };
