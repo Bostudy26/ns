6d09b976f325900d1971ce8c918ea05b8ae6727b
Bo <boprojectbo@gmail.com>
2024-09-14 14:03:29 +0200

    Feat: SpecialOffer + Change: Button | Fix: Hero tailwind

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -1,21 +1,38 @@
 interface ButtonProps {
-  label: string;
+  label: string; 
   iconURL?: string;
+  backgroundColor?: string;
+  textColor?: string;
+  borderColor?: string;
+  fullWidth?: boolean;
 }
+
 const Button = ({
   label,
   iconURL,
-}: ButtonProps) => {
+  backgroundColor,
+  textColor,
+  borderColor,
+  fullWidth,
+} : ButtonProps) => {
   return (
     <button
-      className="flex justify-center items-center gap-2 px-7 py-4 leading-none bg-coral-red rounded-full text-white border-coral-red"
+      className={`flex justify-center items-center gap-2 px-7 py-4 border font-montserrat text-lg leading-none
+      ${
+        backgroundColor
+          ? `${backgroundColor} ${textColor} ${borderColor}`
+          : "bg-coral-red text-white border-coral-red"
+      } rounded-full ${fullWidth && "w-full"}`}
     >
       {label}
-        {iconURL && <img 
+
+      {iconURL && (
+        <img
           src={iconURL}
-          alt="arrow-right"
-          className="ml-2 rounded-full w-5 h-5"
-      />}
+          alt="arrow right icon"
+          className="ml-2 rounded-full bg-white w-5 h-5"
+        />
+      )}
     </button>
   );
 };
